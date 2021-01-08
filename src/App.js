befe7c98d46b481a5bbb83a33baa4a7223cc8bfc
chimdi <nzechimdi@yahoo.com>
2021-01-09 00:04:16 +0100

    commits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [km, setKm] = useState('')
+
+  function convert(){
+    return (km/1.609).toFixed(2)
+  }
+  function handleChange(e){
+    setKm(e.target.value) 
+  }
+  const solved = () => {
+    convert(km)
+    console.log(convert(km))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="main"  onSubmit={(e)=> e.preventDefault()} >
+      <input
+        type="text"
+        value={km}
+        onChange={handleChange}
+      />
+      <button>Convert</button> <br /><br />
+      <h4> {solved()} </h4>
+    </form>
   );
 }
 
